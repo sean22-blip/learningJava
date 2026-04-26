611914a1b15783d6a55ad71ec02f85e29520077f
sean22-blip <iseandxd@gmail.com>
2026-04-26 10:55:25 +0700

    changes to Enum

--- collection_Framework/enumDemo/EnumDemo.java
+++ collection_Framework/enumDemo/EnumDemo.java
@@ -1,4 +1,30 @@
 package collection_Framework.enumDemo;
 
+import java.util.EnumSet;
+
 public class EnumDemo {
+
+    public static void main(String[] args) {
+        // Set<Size> sizes = new HashSet<>();
+        //   sizes.add(Size.SMALL);
+        //  sizes.add(Size.MEDIUM);
+        //  sizes.add(Size.LARGE);
+        //  sizes.add(Size.EXTRALARGE);
+        //  System.out.println(Size.getAllSizes());
+
+        EnumSet<Size> sizes2 = EnumSet.allOf(Size.class);//get all the element of the Size without having to use .add();
+        // System.out.println(sizes2);
+
+        EnumSet<Size> set3 = EnumSet.noneOf(Size.class);
+        set3.add(Size.LARGE);
+        // System.out.println(set3);//can not add anything beside the type of element inside Enum
+
+        EnumSet<Size> set4 = EnumSet.range(Size.MEDIUM, Size.EXTRALARGE);
+        // EnumSet<Size> set4 = EnumSet.range(Size.SMALL, Size.SMALL);
+        // System.out.println(set4);
+        EnumSet<Size> set5 = EnumSet.of(Size.MEDIUM, Size.EXTRALARGE); // only get the element of what is inside the .of()
+        // System.out.println(set5);
+        
+    }
+
 }
--- collection_Framework/enumDemo/Size.java
+++ collection_Framework/enumDemo/Size.java
@@ -1,4 +1,19 @@
 package collection_Framework.enumDemo;
+import java.util.EnumSet;
+import java.util.Set;
+public enum Size {
+    SMALL, MEDIUM, LARGE, EXTRALARGE, XXL;
+     public static Set<Size> getAllSizes(){
+        //  Set<Size> sizes = new HashSet<>();
+        //  sizes.add(Size.SMALL);
+        //  sizes.add(Size.MEDIUM);
+        //  sizes.add(Size.LARGE);
+        //  sizes.add(Size.EXTRALARGE);
+        //  return sizes;
+        return EnumSet.allOf(Size.class);//get all the element of the Size without having to use .add();
 
-public class Size {
+     };
+
+
+
 }

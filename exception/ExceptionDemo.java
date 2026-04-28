2a4d57612be6a3c12181bd9b436daa769b7fb870
sean22-blip <iseandxd@gmail.com>
2026-04-28 20:39:26 +0700

    learning exception

---
+++ exception/ExceptionDemo.java
@@ -0,0 +1,35 @@
+package exception;
+
+import java.util.Scanner;
+
+public class ExceptionDemo {
+public static void main(String[] args) {
+    // System.out.println("Handling Exception");
+    Scanner sc = new Scanner(System.in);
+    // int a = 0;
+    // int b = 5;
+    System.out.println("First line");
+        // System.out.println(result);
+
+
+
+
+
+
+
+
+    //     while(true){
+    //     try{        System.out.print("Enter your name: ");
+    //     String name = sc.nextLine();
+    //     if(!name.matches("[a-zA-Z ]+")){
+    //         System.out.println("Please enter a valid format!!!!");
+    //     }else{
+    //         System.out.println("Successfully entered name!");
+    //         break;
+    //     }
+    // }catch(Exception e){
+    //     System.out.println("Entered the catch exception");  
+    //     System.out.println("Second line");
+    // }
+}
+}
--- random/studentAppDemo.java
+++ random/studentAppDemo.java
@@ -1,14 +1,21 @@
 package random;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Scanner;
 
 public class studentAppDemo {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Map<String, Student> insertStudent = new HashMap<>();
         Student student = new Student("Mey", "1001", 20);
         Student student1 = new Student("Ash", "a2321", 24);
-        Student student2 = new Student("Chloe", "32332", 90);me
+        Student student2 = new Student("Chloe", "32332", 90);
     insertStudent.put("s1", student);
     insertStudent.put("s2", student1);
     insertStudent.put("s3", student2);

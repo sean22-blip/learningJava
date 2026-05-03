ab25050465c466fcb89142439f291f62569b72d4
sean22-blip <iseandxd@gmail.com>
2026-05-03 10:20:28 +0700

    learnin more about check and unchecked exception

--- exception/Handling_Exception.java
+++ exception/Handling_Exception.java
@@ -1,7 +1,14 @@
 package exception;
 
 public class Handling_Exception {
-public static void main(String[] args) {
-    
-}
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+            // This will never be reached
+        } catch (ArithmeticException e) {
+            System.out.println("Divide by zero.");
+        } catch (Exception e) {
+            System.out.println("General error.");
+        }//Tip: Always put Exception (the general one) at the end.
+    }
 }
--- exception/TestingException.java
+++ exception/TestingException.java
@@ -1,25 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.classfile.FieldElement;
 import java.util.Scanner;
+
 public class TestingException{
 public static void main(String[] args) {
-//     File file = new File("Downloads/person");
-//     //the problem is that when a file is not found when we are trying to try-catch
-//     try{
-//         Scanner scanner = new Scanner(file);//checking exception at compile time 
-// }catch(FileNotFoundException e){
-//     e.printStackTrace();
-// }
-// checkAge(-1);
-checkAge(20);
-// }
-
-}
-static public void checkAge(int age) {
-    if (age < 0) {
-        throw new IllegalArgumentException("Age cannot be negative!"); // stops here 💥
+File file = new File("C:\\TempFolder\\person.txt");
+try {
+    Scanner scanner = new Scanner(file);
+    while(scanner.hasNext()){
+        System.out.println(scanner.next());
     }
-    System.out.println("Age is: " + age); // ← this NEVER runs
+    scanner.close();
+} catch (FileNotFoundException e) {
+    e.printStackTrace();
+}finally{
+    System.out.println("Successfully exiting the file checking!!!");
+}
 }
 }

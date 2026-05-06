d4889bb982aaa7ed150d4a1d3398f2cedec361e3
sean22-blip <iseandxd@gmail.com>
2026-05-06 12:57:44 +0700

    learning throw and throws

--- exception/ThrowDemo.java
+++ exception/ThrowDemo.java
@@ -1,10 +1,20 @@
 package exception;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class ThrowDemo
 {
-    public static void main(String[] args) {
+    // public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws IOException, RuntimeException{//any exception works
         int age = 17;
         if (age < 18) {
-            throw new RuntimeException("Age under 18 cannot vote!!!");
+            // throw new Exception("Age under 18 cannot vote!!!");
+            throw new FileNotFoundException("Age under 18 cannot vote!!!");
+        }
+
+        if(age > 50){
+            throw new RuntimeException("You are too old to vote!!");
         }
         System.out.println("can vote!!");
     }
--- exception/ThrowsDemo.java
+++ exception/ThrowsDemo.java
@@ -1,7 +1,45 @@
 package exception;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ThrowsDemo {
-public static void main(String[] args) {
-    
-}
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // try {
+        //     readFile2();
+        // } catch (FileNotFoundException e) {
+        //     e.printStackTrace();
+        //     System.out.println(">> Your file is not found <<");
+        // }
+        // readFile();
+        // System.out.println("=============================");
+        // readFile2();
+        readFile();
+    }
+
+    public static void readFile() {
+        File file = new File("C:\\Users\\CADT\\OneDrive - Cambodia Academy of Digital Technology\\Desktop//ranewewdom.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()) {
+                System.out.println(sc.next());
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("=== File is not found ===");
+        }
+    }
+
+    public static void readFile2() throws FileNotFoundException {
+        File file = new File("C:\\Users\\CADT\\OneDrive - Cambodia Academy of Digital Technology\\Desktop//randomfdfaf.txt");
+        Scanner sc = new Scanner(file);
+        while (sc.hasNext()) {
+            System.out.println(sc.next());
+        }
+        sc.close();
+    }
+
 }

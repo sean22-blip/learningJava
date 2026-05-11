6b95ac5ac76d43bfd306483ec98eddbeaef898cd
sean22-blip <iseandxd@gmail.com>
2026-05-11 19:44:44 +0700

    finish learning inputstream

--- file/fileInputStream/InputStreamDemo.java
+++ file/fileInputStream/InputStreamDemo.java
@@ -4,19 +4,26 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 
 public class InputStreamDemo {
     public static void main(String[] args) {
         byte[] myData = new byte[100];
         try {
             InputStream inputStream = new FileInputStream("/home/isean/Desktop/data.txt");
-            System.out.println(inputStream.available());
+
+
+        //    int letter = inputStream.read();
+        //    System.out.println((char)letter);
+            // inputStream.read();
+            inputStream.skip(14);
             inputStream.read(myData);
-//            System.out.println(Arrays.toString(myData));
-//            for(byte b : myData){
-//                System.out.print((char) b);
-//            }
+
+            System.out.println(inputStream.available());
+            // inputStream.read(myData);
+                    //    System.out.println(Arrays.toString(myData));
+        //    for(byte b : myData){
+        //        System.out.print(b);
+        //    }
             String data = new String(myData);
             System.out.print(data);
 //            System.out.println(inputStream.available());
@@ -24,6 +31,6 @@ public class InputStreamDemo {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        } 
     }
 }
